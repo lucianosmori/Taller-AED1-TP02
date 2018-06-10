c19dd598a22ecf14a9bd0ec2dcc16b970253dbbb
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2018-06-09 22:55:52 -0300

    Update QueueActions.java

--- Escritorio/Test/src/estructuras/cola/QueueActions.java
+++ Escritorio/Test/src/estructuras/cola/QueueActions.java
@@ -1,22 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package estructuras.cola;
+import excepciones.EmptyQueueException;
 import test.*;
 
-/**
- *
- * @author ncalumno01
- */
+
 public interface QueueActions {
     
-    	boolean isEmpty();
+    boolean isEmpty();
 
 	void reset();
 
 	void enqueue(Object o);
+	
+	void print();
 
 	Object first() throws EmptyQueueException;
 
